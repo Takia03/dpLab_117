54d85f60145b6bb76be9d11607fdcee8b370f88d
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-06 23:21:31 +0600

    BankAccount class updated

--- Lab 10/src/BankAccount.java
+++ Lab 10/src/BankAccount.java
@@ -1,44 +1,39 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class BankAccount {
+class BankAccount {
     private String accountNumber;
     private double balance;
     private List<String> transactionHistory;
 
-    public BankAccount(String accountNumber, double balance) {
+    public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
-        this.balance = balance;
+        this.balance = initialBalance;
         this.transactionHistory = new ArrayList<>();
+        transactionHistory.add("Account created with balance: $" + initialBalance);
     }
 
-    public void deposite(double amount){
+    public String getAccountNumber() { return accountNumber; }
+    public double getBalance() { return balance; }
+    public List<String> getTransactionHistory() { return transactionHistory; }
+
+    public void deposit(double amount, EventBus eventBus) {
+        if (amount <= 0) {
+            System.out.println("Deposit amount must be positive!");
+            return;
+        }
         balance += amount;
-        transactionHistory.add("Deposite:" + amount);
+        transactionHistory.add("Deposited: $" + amount);
+        eventBus.publish(new Event("Deposit", "Account " + accountNumber + " deposited $" + amount));
     }
 
-    public void withdraw(double amount){
-    if(balance >= amount){
+    public void withdraw(double amount, EventBus eventBus) {
+        if (amount > balance) {
+            System.out.println("Insufficient funds!");
+            return;
+        }
         balance -= amount;
-        transactionHistory.add("Withdraw:" + amount);
-       }else {
-        System.out.println("Insufficient balance");
-       }
-     }
-
-     public double getBalance(){
-        return balance;
-     }
-
-
-     public List<String> getTransactionHistory(){
-        return transactionHistory;
-     }
-
-     public String getAccountNumber(){
-        return accountNumber;
-     }
-
+        transactionHistory.add("Withdrew: $" + amount);
+        eventBus.publish(new Event("Withdraw", "Account " + accountNumber + " withdrew $" + amount));
+    }
 }
-
-
-
